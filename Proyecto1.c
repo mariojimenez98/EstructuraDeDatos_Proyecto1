63782946dc338485a0606bef61d281be36beb883
Mario Jimenez <50642172+mariojimenez98@users.noreply.github.com>
2020-09-26 15:14:29 -0500

    First Commit
    The project has been created

---
+++ Proyecto1.c
@@ -0,0 +1,16 @@
+/********************************************
+    Nombre de Alumnos: Mario Ivan Jimenez Alfaro #727740
+                       Benjamin
+                       Alejandro
+                       Carlos
+    Fecha: 10 de Octubre 2020
+    Materia: Estructura de Datos
+    Programa: Proyecto 1
+    Descripcion: Estudio para Mobil Mexico
+********************************************/
+
+#include <stdio.h>
+
+void main() {
+    printf("Hello World!");
+}
