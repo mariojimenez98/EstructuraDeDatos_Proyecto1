0fc5f62ddc80904ca3ba26ef2122ccde468e9406
Mario Jimenez <mario.jimenez@u-erre.mx>
2020-10-10 12:46:32 -0500

    Ultimos cambios

--- Proyecto1.c
+++ Proyecto1.c
@@ -1,7 +1,6 @@
 /********************************************
     Nombre de Alumnos: Mario Ivan Jimenez Alfaro #727740
                        Benjamin Gil Flores #727211
-                       Alejandro Sada Prendes #725958
                        Carlos Alanis #732148
     Fecha: 10 de Octubre 2020
     Materia: Estructura de Datos
